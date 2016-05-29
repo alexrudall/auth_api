f1a4bc5c98523fe591dcc84bf31f09a578d53261
Alex Rudall <alexrudall@gmail.com>
2016-05-29 18:44:44 +0100

    UsersController tests passing

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module AuthApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('app/commands')
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,34 +3,35 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:valid)
+    @token = AuthenticateUser.call(@user.email, 'password').result
   end
 
   test "should get index" do
-    get users_url
+    get users_url, headers: { authorization: @token }
     assert_response :success
   end
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, password_digest: @user.password_digest, username: @user.username } }
+      post users_url, params: { user: { email: @user.email, password: 'password', username: @user.username } }, headers: { authorization: @token }
     end
 
     assert_response 201
   end
 
   test "should show user" do
-    get user_url(@user)
+    get user_url(@user), headers: { authorization: @token }
     assert_response :success
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, password_digest: @user.password_digest, username: @user.username } }
+    patch user_url(@user), params: { user: { email: @user.email, password: 'password', username: @user.username } }, headers: { authorization: @token }
     assert_response 200
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
-      delete user_url(@user)
+      delete user_url(@user), headers: { authorization: @token }
     end
 
     assert_response 204
