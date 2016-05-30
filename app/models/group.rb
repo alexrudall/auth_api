6f32e23f6e33eb7113880842a6543a45a9f81ba8
Alex Rudall <alexrudall@gmail.com>
2016-05-30 12:55:25 +0100

    Validate group name uniqueness

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,3 @@
 class Group < ApplicationRecord
+  validates_uniqueness_of :name
 end
---
+++ test/models/group_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class GroupTest < ActiveSupport::TestCase
+  def setup
+    @group = groups(:one)
+  end
+
+  test "name should be unique" do
+     duplicate_group = @group.dup
+     assert_not duplicate_group.valid?
+  end
+end
